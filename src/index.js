5efc42a91a376940d16b45f338475c2b42652ff6
Tom Yancey <thyancey@gmail.com>
2020-05-24 01:24:55 -0400

    got a lot of wacky spawn stuff, click events, etc happening

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
+import img_blood from "./assets/blood.png";
 
 import { spawn_update, spawn_preload, spawn_create } from './spawn-manager';
 
 const config = {
   type: Phaser.AUTO,
   parent: "game-container",
-  width: 800,
-  height: 600,
+  width: 900,
+  height: 500,
   physics: {
       default: 'arcade',
       arcade: {
-          gravity: { y: 300 },
+          gravity: { y: 800 },
           debug: false
       }
   },
@@ -23,25 +24,70 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let emitter;
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('logo', logoImg);
+  this.load.image('blood', img_blood);
   spawn_preload(this);
 
 }
 
+function onObjectClicked(pointer, gameObject){
+  // gameObject.anims.play('raccoon_dead');
+  // console.log('clicked it', gameObject);
+  // gameObject.kill();
+
+  emitter.setPosition(pointer.x, pointer.y);
+  // emitter.reset();
+  emitter.explode(20);
+  emitter.visible = true;
+}
+
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  // const logo = this.add.image(400, 150, "logo");
 
   spawn_create(this);
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
+  
+  this.input.on('gameobjectdown', onObjectClicked);
+
+  // this.tweens.add({
+  //   targets: logo,
+  //   y: 450,
+  //   duration: 2000,
+  //   ease: "Power2",
+  //   yoyo: true,
+  //   loop: -1
+  // });
+
+  var particles = this.add.particles('blood');
+/*
+  emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD',
+  });
+*/
+
+// emitter = particles.createEmitter({
+//   speed: 100,
+//   scale: { start: 1, end: 0 },
+//   blendMode: 'ADD',
+//   maxParticles: 10
+// });
+
+  emitter = particles.createEmitter({
+    // speed: 100,
+    // scale: { start: 1, end: 0 },
+    // active:false,
+    visible: false,
+    blendMode: 'SCREEN',
+    speed: { min: -400, max: 400 },
+    angle: { min: 0, max: 360 },
+    scale: { start: 1, end: 0 },
+    lifespan: 500,
+    gravityY: 1000
   });
 }
 
--- src/spawn-manager.js
+++ src/spawn-manager.js
@@ -1,44 +1,125 @@
 import Phaser from "phaser";
 import img_raccoonTest from "./assets/raccoon1.png";
+import img_bar from "./assets/bar.png";
 
-const spawnFrequency = 100;
-let curTicker = 0;
+
+const SPAWN_MIN = 1000;
+const SPAWN_MAX = 10;
+
+let spawnFrequency = 50;
+let curTicker = 1000;
 
 let enemies;
+let platforms;
 
-const spawnThing = () => {
-  let enemy = enemies.create(0, 16, 'raccoon');
-  enemy.setBounce(.5);
+let spawnSlider;
+
+const getValue = position => {
+  var minp = 0;
+  var maxp = 100;
+
+  var minv = Math.log(SPAWN_MIN);
+  var maxv = Math.log(SPAWN_MAX);
+  
+  var scale = (maxv-minv) / (maxp-minp);
+  
+  return Math.exp(minv + scale*(position-minp));
+}
+
+const onSpawnSlider = sliderVal => {
+  spawnFrequency = Math.round(getValue(sliderVal));
+  
+  const sliderPercent = sliderVal / 100;
+  document.querySelector('#spawn-display').innerHTML = `${sliderVal}%`;
+}
+
+const spawnRaccoon = () => {
+  let enemy = enemies.create(0, 50, 'raccoon_walk')
+  // let enemy = enemies.create(0, 50, 'raccoon_walk').setInteractive();
+  enemy.isAlive = true;
+  enemy.kill = () => {
+    console.log('YOU KILLED ME');
+    enemy.anims.play('raccoon_dead');
+    enemy.body.setDrag(500);
+    enemy.isAlive = false;
+  }
+
+  enemy.setBounce(.4);
   enemy.setCollideWorldBounds(true);
-  enemy.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+  enemy.setVelocity(Phaser.Math.Between(200, 800), 20);
+  //- squeeze in hit box from edge of sprite
+  enemy.body.setSize(60,70);
+  enemy.body.offset.x = 35;
+  enemy.body.offset.y = 30;
   enemy.allowGravity = false;
-  enemy.anims.play('right');
+  enemy.anims.play('raccoon_walk');
+  enemy.setInteractive();
+
+  enemy.on('pointerdown', (thing) => {
+    enemy.kill();
+  });
 }
 
 export const spawn_preload = (context) => {
   context.load.spritesheet('raccoon', img_raccoonTest, { frameWidth: 110, frameHeight: 110 });
+  context.load.image('bar', img_bar);
 }
 
-
 export const spawn_create = (context) => {
-  // context.load.spritesheet("raccoon", img_raccoonTest, { frameWidth: 110, frameHeight: 110 });
-  
+  spawnSlider = document.querySelector('#spawn-slider');
+  // spawnSlider.addEventListener('change', (e) => {
+  //   onSpawnSlider(e.target.value);
+  // })
+  spawnSlider.oninput = (e) => {
+    onSpawnSlider(e.target.value);
+  }
+
+  //- make the level
+  platforms = context.physics.add.staticGroup();
+
+  platforms.create(450, 500, 'bar').setScale(4, .5).refreshBody();
+  platforms.create(800, 480, 'bar');
+  platforms.create(50, 220, 'bar').setScale(-1, .5).refreshBody();
+
+  //- init the enemy stuff
 
   enemies = context.physics.add.group();
 
   context.anims.create({
-    key: 'right',
+    key: 'raccoon_walk',
     frames: context.anims.generateFrameNumbers('raccoon', { start: 0, end: 1 }),
     frameRate: 10,
     repeat: -1
   });
+
+  context.anims.create({
+    key: 'raccoon_dead',
+    frames: [ { key: 'raccoon', frame: 2 } ],
+    frameRate: 10
+  });
+  
+  context.physics.add.collider(enemies, platforms);
+}
+
+const checkForJump = (entity, chance) => {
+
+  if(entity.isAlive && entity.body.touching.down && Math.random() < chance){
+    entity.setVelocityX((1 + Math.random()) * entity.body.velocity.x);
+    entity.setVelocityY(Math.random() * - 300);
+  }
 }
 
 export const spawn_update = () => {
   enemies.children.each(entity => {
-    entity.flipX = entity.body.velocity.x < 0;
-  });
+    if(entity.flipX){
+      if(entity.body.velocity.x > 0) entity.flipX = false;
+    }else{
+      if(entity.body.velocity.x < 0) entity.flipX = true;
+    }
 
+    checkForJump(entity, .05);
+  });
   checkSpawn();
 }
 
@@ -48,6 +129,7 @@ export const checkSpawn = () => {
   curTicker++;
   if(curTicker > spawnFrequency){
     curTicker = 0;
-    spawnThing();
+    // spawnFrequency *= .8;
+    spawnRaccoon();
   }
 }
