5e0515688e3b57a197576622d20854681b5c9c19
Tom Yancey <thyancey84@gmail.com>
2021-12-18 12:21:49 -0500

    add gh-pages, cleanup repo stuff

--- src/page/index.js
+++ src/page/index.js
@@ -3,7 +3,7 @@ import { renderHtml } from '../utils';
 export const createHeader = () => {
 
   const logoHtml = `
-    <h2>Phaser 3 test</h2>
+    <h2>Raccoon Trapper</h2>
     <div class="description">
       <p>Feed the raccoons and give them hugs!</p>
       <p>Kick the ones who don't love you!</p>
